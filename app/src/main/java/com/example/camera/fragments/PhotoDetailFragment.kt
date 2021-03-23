0e0666493cf75f5778e2227b91856c052c3466cc
Alessandro Cancelliere <alessandro@cancelliere.info>
2021-03-23 14:03:05 +0100

    added: compass and save image into gallery

--- app/src/main/java/com/example/camera/fragments/PhotoDetailFragment.kt
+++ app/src/main/java/com/example/camera/fragments/PhotoDetailFragment.kt
@@ -2,23 +2,21 @@ package com.example.camera.fragments
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.pm.PackageManager
 import android.content.res.ColorStateList
 import android.graphics.Bitmap
-import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.*
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.view.animation.RotateAnimation
 import android.widget.Toast
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.DrawableCompat
 import androidx.core.graphics.drawable.toBitmap
-import androidx.core.graphics.toColor
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.camera.R
@@ -26,18 +24,25 @@ import com.example.camera.dialogs.EditTextDialogFragment
 import com.example.camera.interfaces.EditTextDialog
 import com.example.camera.utils.CommonUtils.BASE_URI
 import com.example.camera.utils.CommonUtils.PIC_URI
+import com.example.camera.utils.Compass
+import com.example.camera.utils.SOTWFormatter
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import ja.burhanrashid52.photoeditor.PhotoEditor
 import kotlinx.android.synthetic.main.fab_layout.*
 import kotlinx.android.synthetic.main.fragment_photo_detail.*
-import java.lang.Exception
-
 
 class PhotoDetailFragment : Fragment(), View.OnClickListener, EditTextDialog,
-    BottomNavigationView.OnNavigationItemSelectedListener, PhotoEditor.OnSaveListener {
+    BottomNavigationView.OnNavigationItemSelectedListener, PhotoEditor.OnSaveListener,
+    Compass.CompassListener {
 
-    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(requireContext(), R.anim.from_bottom_anim) }
-    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(requireContext(), R.anim.to_bottom_anim) }
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(
+        requireContext(),
+        R.anim.from_bottom_anim
+    ) }
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(
+        requireContext(),
+        R.anim.to_bottom_anim
+    ) }
 
     private lateinit var pictureUri : Uri
     private lateinit var editor : PhotoEditor
@@ -48,6 +53,26 @@ class PhotoDetailFragment : Fragment(), View.OnClickListener, EditTextDialog,
     private var unidirectionalArrow = R.drawable.unidirectional_arrow
     private var bidirectionalArrow = R.drawable.bidirectional_arrow
     private var line = R.drawable.line
+    private var compass: Compass? = null
+    private var sotwFormatter: SOTWFormatter? = null
+    private var currentAzimuth = 0f
+    private var imgTitle = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        colorText = requireContext().getColor(R.color.black)
+        sotwFormatter = SOTWFormatter(requireContext())
+
+        arguments?.getString(PIC_URI)?.apply {
+            pictureUri = Uri.parse("$BASE_URI$this")
+        }
+
+        imgTitle = arguments?.getString(PIC_URI)!!
+
+        compass = Compass(requireContext())
+        compass?.setListener(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -61,18 +86,13 @@ class PhotoDetailFragment : Fragment(), View.OnClickListener, EditTextDialog,
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
 
-        arguments?.getString(PIC_URI)?.apply {
-            pictureUri = Uri.parse("$BASE_URI$this")
-            photoEditorView.source.setImageURI(pictureUri)
-        }
+        photoEditorView.source.setImageURI(pictureUri)
 
         editor = PhotoEditor
             .Builder(requireContext(), photoEditorView)
             .build()
 
         fab.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.black))
-
-        colorText = requireContext().getColor(R.color.black)
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this)
         fab.setOnClickListener(this)
@@ -82,6 +102,17 @@ class PhotoDetailFragment : Fragment(), View.OnClickListener, EditTextDialog,
         fab_4.setOnClickListener(this)
         fab_5.setOnClickListener(this)
         fab_6.setOnClickListener(this)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        compass?.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        compass?.stop()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -100,11 +131,24 @@ class PhotoDetailFragment : Fragment(), View.OnClickListener, EditTextDialog,
                     arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
                     REQUEST_WRITE_PERMISSION
                 )
-                else editor.saveAsFile(pictureUri.toString(), this)
+                else {
+                        editor.saveAsFile(pictureUri.toString(), this)
+                }
             }
         }
         return super.onOptionsItemSelected(item)
     }
+
+    private fun saveImage(pic: String){
+        val uriPic = Uri.parse("file:///$pic")
+        val bitmap = changeUriToBitmap(uriPic)
+        MediaStore.Images.Media.insertImage(requireContext().contentResolver,
+            bitmap,
+            imgTitle,
+            "empty description")
+    }
+
+    private fun changeUriToBitmap(uri: Uri) = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, uri)
 
     @SuppressLint("MissingPermission")
     override fun onRequestPermissionsResult(
@@ -122,7 +166,9 @@ class PhotoDetailFragment : Fragment(), View.OnClickListener, EditTextDialog,
     }
 
     override fun onSuccess(imagePath: String) {
+        saveImage(pictureUri.toString())
         findNavController().popBackStack()
+        Toast.makeText(requireContext(), "Foto salvata con successo", Toast.LENGTH_SHORT).show()
     }
 
     override fun onFailure(exception: Exception) {
@@ -131,21 +177,21 @@ class PhotoDetailFragment : Fragment(), View.OnClickListener, EditTextDialog,
 
     private fun editWithLine(editor: PhotoEditor){
         val arrow = ContextCompat.getDrawable(requireContext(), line)
-        val bitmap = arrow?.toBitmap(stickerWidth, stickerHeight,Bitmap.Config.ARGB_8888)
+        val bitmap = arrow?.toBitmap(stickerWidth, stickerHeight, Bitmap.Config.ARGB_8888)
         editor.setOpacity(0)
         editor.addImage(bitmap)
     }
 
     private fun editWithUnidirectionalArrow(editor: PhotoEditor){
         val arrow = ContextCompat.getDrawable(requireContext(), unidirectionalArrow)
-        val bitmap = arrow?.toBitmap(stickerWidth, stickerHeight,Bitmap.Config.ARGB_8888)
+        val bitmap = arrow?.toBitmap(stickerWidth, stickerHeight, Bitmap.Config.ARGB_8888)
         editor.setOpacity(0)
         editor.addImage(bitmap)
     }
 
     private fun editWithBidirectionalArrow(editor: PhotoEditor){
         val arrow = ContextCompat.getDrawable(requireContext(), bidirectionalArrow)
-        val bitmap = arrow?.toBitmap(stickerWidth, stickerHeight,Bitmap.Config.ARGB_8888)
+        val bitmap = arrow?.toBitmap(stickerWidth, stickerHeight, Bitmap.Config.ARGB_8888)
         editor.setOpacity(0)
         editor.addImage(bitmap)
     }
@@ -167,7 +213,11 @@ class PhotoDetailFragment : Fragment(), View.OnClickListener, EditTextDialog,
 
     override fun editTextOnPic(text: String) {
         if(text.isEmpty()){
-            Toast.makeText(requireContext(), "Testo vuoto! Impossibile inserire la nota.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                requireContext(),
+                "Testo vuoto! Impossibile inserire la nota.",
+                Toast.LENGTH_SHORT)
+                .show()
         } else {
             editor.addText(text, colorText)
         }
@@ -212,66 +262,91 @@ class PhotoDetailFragment : Fragment(), View.OnClickListener, EditTextDialog,
     }
 
     private fun setRedColor(){
-        line = R.drawable.line_red
-        unidirectionalArrow = R.drawable.unidirectional_arrow_red
-        bidirectionalArrow = R.drawable.bidirectional_arrow_red
-        fab.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.photo_brush_red))
-        colorText = requireContext().getColor(R.color.photo_brush_red)
+        refreshStickers(R.drawable.line_red,
+                        R.drawable.unidirectional_arrow_red,
+                        R.drawable.bidirectional_arrow_red,
+                        R.color.photo_brush_red)
 
         onFabButtonClicked()
     }
 
     private fun setGreenColor(){
-       line = R.drawable.line_green
-       unidirectionalArrow = R.drawable.unidirectional_arrow_green
-        bidirectionalArrow = R.drawable.bidirectional_arrow_green
-        colorText = requireContext().getColor(R.color.photo_brush_green)
-        fab.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.photo_brush_green))
+        refreshStickers(R.drawable.line_green,
+                        R.drawable.unidirectional_arrow_green,
+                        R.drawable.bidirectional_arrow_green,
+                        R.color.photo_brush_green)
 
         onFabButtonClicked()
     }
 
     private fun setBlueColor(){
-        line = R.drawable.line_blue
-        unidirectionalArrow = R.drawable.unidirectional_arrow_blue
-        bidirectionalArrow = R.drawable.bidirectional_arrow_blue
-        colorText = requireContext().getColor(R.color.photo_brush_blue)
-        fab.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.photo_brush_blue))
+        refreshStickers(R.drawable.line_blue,
+                        R.drawable.unidirectional_arrow_blue,
+                        R.drawable.bidirectional_arrow_blue,
+                        R.color.photo_brush_blue)
 
         onFabButtonClicked()
     }
 
     private fun setYellowColor(){
-        line = R.drawable.line_yellow
-        unidirectionalArrow = R.drawable.unidirectional_arrow_yellow
-        bidirectionalArrow = R.drawable.bidirectional_arrow_yellow
-        colorText = requireContext().getColor(R.color.photo_brush_yellow)
-        fab.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.photo_brush_yellow))
+        refreshStickers(R.drawable.line_yellow,
+                        R.drawable.unidirectional_arrow_yellow,
+                        R.drawable.bidirectional_arrow_yellow,
+                        R.color.photo_brush_yellow)
 
         onFabButtonClicked()
     }
 
     private fun setBlackColor(){
-        line = R.drawable.line
-        unidirectionalArrow = R.drawable.unidirectional_arrow
-        bidirectionalArrow = R.drawable.bidirectional_arrow
-        colorText = requireContext().getColor(R.color.black)
-        fab.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.black))
+        refreshStickers(R.drawable.line,
+                        R.drawable.unidirectional_arrow,
+                        R.drawable.bidirectional_arrow,
+                        R.color.black)
 
         onFabButtonClicked()
     }
 
     private fun setPurpleColor(){
-        line = R.drawable.line_purple
-        unidirectionalArrow = R.drawable.unidirectional_arrow_purple
-        bidirectionalArrow = R.drawable.bidirectional_arrow_purple
-        colorText = requireContext().getColor(R.color.photo_deep_purple)
-        fab.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.photo_brush_purple))
+        refreshStickers(R.drawable.line_purple,
+                        R.drawable.unidirectional_arrow_purple,
+                        R.drawable.bidirectional_arrow_purple,
+                        R.color.photo_deep_purple)
 
         onFabButtonClicked()
     }
 
+    private fun refreshStickers(line: Int, unidirArrow: Int, bidirArrow: Int, color: Int){
+        this.line = line
+        this.unidirectionalArrow = unidirArrow
+        this.bidirectionalArrow = bidirArrow
+        this.colorText = requireContext().getColor(color)
+        this.fab.backgroundTintList = ColorStateList.valueOf(resources.getColor(color))
+    }
+
     companion object {
         const val REQUEST_WRITE_PERMISSION = 1001
+    }
+
+
+    override fun onNewAzimuth(azimuth: Float) {
+        adjustArrow(azimuth)
+    }
+
+    private fun adjustArrow(azimuth: Float) {
+        Log.d(
+            "adjustArrow",
+            "will set rotation from " + currentAzimuth + " to "
+                    + azimuth
+        )
+        val an: Animation = RotateAnimation(
+            -currentAzimuth, -azimuth,
+            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+            0.5f
+        )
+        currentAzimuth = azimuth
+        an.duration = 500
+        an.repeatCount = 0
+        an.fillAfter = true
+        img_compass.startAnimation(an)
     }
 }
